677efee9a1138e14d3d016621b38f8601e15d10c
Adyson Vieira <contato.adyson@gmail.com>
2022-05-12 12:51:05 -0300

    finalizando menu desktop

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+// Adicionando o evento de scroll diretamente na janela e executando logo no carregamento
 window.addEventListener('scroll', onScroll)
 
 onScroll()
 
+// Função que executa as funções de scroll
 function onScroll() {
     scrollNav()
     scrollButtonTop()
 }
 
+// Função para adicionar e remover a classe 'scroll' no menu, fazendo o menu trocar o backgroud quando rolar o scroll
 function scrollNav() {
     if(scrollY == 0) {
         navigation.classList.remove('scroll')
@@ -17,6 +20,7 @@ function scrollNav() {
     }
 }
 
+// Função para adicionar e remover a classe 'to-top' no btnTop, fazendo o botão aparecer depois que o scroll chega a um determinado ponto
 function scrollButtonTop() {
     if(scrollY >= 300) {
         btnTop.classList.add('to-top')
@@ -27,14 +31,17 @@ function scrollButtonTop() {
     }
 }
 
+// Função para abrir o menu-mobile
 function openMenu() {
     document.body.classList.add('menu-expanded')
 }
 
+// Função para fechar o menu-mobile
 function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
 
+//lib para transição dos itens durante o scroll
 ScrollReveal({
     origin:'top',
     distance: '30px',
